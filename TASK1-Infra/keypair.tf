dff1f9fdfcb7f1e185aeee71ebaa149735090777
rbmcnutt <rbmcnutt+github@gmail.com>
2022-09-20 08:54:04 -0500

    Update keypair.tf

--- TASK1-Infra/keypair.tf
+++ TASK1-Infra/keypair.tf
@@ -1,5 +1,5 @@
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmUWhB4ykz/gwxWAcMflefrH2sS2NJIG4qnHfbFQ4bBFiBiB9n8B5m/cr5KL+4bhR82pu+rheK4nuJj0YGbJWhvAxOs/AdWn9A40U6JDMEYBUv+iIKl8Jp3qhzrhvFCuSte1v2JU3K/lzlFA/jKcDkGB/UxBYrHKdfif7Dpi5EgBZLuO7AlYE1jdUXqRHyyutWAkQfvwOccRZ7MHBCimQrAJqatFbck15MkBL07gkf3sI3hu13nnxd5M9AAN/uUyxeeK0+bJukquI1CLJy1iANDxc1RXfsaJ5q/hl+DEVWOUmxiev03S2vicKBqhJhxvTEyep8kxEMddpWTmoipMdSyvvKXBUyUpIqdMsICrKDfbKx/45YU7TXT/dC1rjX9Bjv5k71iHhkPAm60WOa/sP6mFUxtChMEg2yIBg1je99Xe+MyWFjWieVl/6XS7l3cgf/6VQzUUjFWcQVZvblm9vfGwEQ1itGl89v2ye7dc44Ktj1xPHc4pPrA/AGwUN09TU= thomasbeaumont@Toms-Mac-Mini-Bureau.local"
+  public_key = var.my_ssh_public_key
 }
