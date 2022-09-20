22441693a97ddf1b7bd7d5576d05bf9f9424c30e
rbmcnutt <rbmcnutt+github@gmail.com>
2022-09-20 18:25:12 -0500

    Update variables.tf

--- TASK1-Infra/variables.tf
+++ TASK1-Infra/variables.tf
@@ -1,14 +1,14 @@
 
 variable "my_ip_address" {
   type = string
-  default = "###.###.###.###/32" #Use google to "fin my ip addresss" Yse the IPv4 format with /32 for the mask
+  default = "###.###.###.###/32" # Use google to "find my ip addresss" Yse the IPv4 format with /32 for the mask
 }
 
 variable "my_bucket_name" {
   type = string
-default = "wizdemo9999"  #Must be globally unique in AWS
+default = "wizdemo9999"  # Must be globally unique in AWS
 }
 
 variable "my_ssh_public_key"{
   type = string
-  default = "ENTER_STRING_HERE" #use a secure string. Do not check this file into the repo.
+  default = "ENTER_STRING_HERE" # Use a secure string. Do not check this file into the repo.
